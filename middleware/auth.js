32e64dde16228ae46fa114bb342a37885c3595e3
gskebin <gskebin4@gmail.com>
2021-11-17 17:36:02 +0900

    async 추가

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 const {errMsg } = require('../util/errorHandle')
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
     try {    
       const authKey = req.headers[process.env.API_KEY];
       if (authKey !== process.env.API_KEY_VALUE) {
